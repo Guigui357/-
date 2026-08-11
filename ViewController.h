c9c04e22b278c688ea498a2b882c524d3cea8b08
Guigui357 <zebuchinho77@gmail.com>
2026-08-11 18:39:33 -0300

    Add ViewController interface with properties and method

---
+++ ViewController.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *jailbreakButton;
+@property (weak, nonatomic) IBOutlet UITextView *logTextView;
+- (void)appendLog:(NSString *)format, ...;
+@end
