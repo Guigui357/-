1b1ca4efb6b99a3ae4f06a395e41deb20c8ee4ca
Guigui357 <zebuchinho77@gmail.com>
2026-08-11 18:42:27 -0300

    Add IBOutlet properties for jailbreakButton and logTextView

--- ViewController.h
+++ ViewController.h
@@ -1,7 +1,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+
 @property (weak, nonatomic) IBOutlet UIButton *jailbreakButton;
 @property (weak, nonatomic) IBOutlet UITextView *logTextView;
+
 - (void)appendLog:(NSString *)format, ...;
+
 @end
