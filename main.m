f6d54a5c3d4613c42984372e0aebaed770ff7a2b
Guigui357 <zebuchinho77@gmail.com>
2026-08-11 18:56:12 -0300

    Initialize main.m with app delegate setup

---
+++ main.m
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
