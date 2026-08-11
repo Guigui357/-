be4b45263d7d4ead52d2def8b54924f73b73c1fe
Guigui357 <zebuchinho77@gmail.com>
2026-08-11 18:52:59 -0300

    Remove IBOutlet properties from ViewController
    Removed IBOutlet properties for jailbreakButton and logTextView.

--- ViewController.h
+++ ViewController.h
@@ -1,10 +1,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-@property (weak, nonatomic) IBOutlet UIButton *jailbreakButton;
-@property (weak, nonatomic) IBOutlet UITextView *logTextView;
-
 - (void)appendLog:(NSString *)format, ...;
-
 @end
